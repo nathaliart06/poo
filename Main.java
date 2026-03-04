22798e1becb7c137ddeff099f26df31a1ba91355
nathaliart06 <nathalia.ramostron@campusucc.edu.co>
2026-03-04 01:15:33 +0000

    Ejercicio orientada a objetos

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main (String [] args ) {
+      // Instanciacion
+      Celular miTelefono = new Celular ();
+      // Acceso a atributos y metodos usando el punto (.)
+      miTelefono.marca = " Samsung ";
+      miTelefono.llamar("123");
+    }
+}
